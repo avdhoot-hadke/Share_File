fd735a67182a71be67dc45eb1f769cef9d8d09c5
avdhoot <avdhoothadke@gmail.com>
2025-12-16 22:45:12 +0530

    backend done

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,17 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const token = req.header('x-auth-token');
-
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ msg: "No token, authorization denied" });
     }
+
+    const token = authHeader.split(" ")[1];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
         next();
     } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+        return res.status(401).json({ msg: "Token is not valid" });
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ import authRoutes from './routes/authRoute.js';
 import fileRoutes from './routes/fileRoute.js';
 
 connectDB();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/files', fileRoutes);
@@ -24,3 +24,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
